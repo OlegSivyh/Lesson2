12526275586f2032a1921ed7011aa458c241d2d1
OlegSivyh <Oleg.sivyh@gmail.com>
2022-05-23 14:35:51 +0300

    added task2

---
+++ Task2.js
@@ -0,0 +1,5 @@
+let FirstNumber = prompt ("Enter the first number");
+let SecondNumber = prompt("Enter the second number");
+let sum = (Number(FirstNumber) +  Number(SecondNumber));
+alert("Your result" + " " + ":" + " " + (sum));
+
